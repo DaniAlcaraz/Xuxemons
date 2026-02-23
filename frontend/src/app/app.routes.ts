dd80213ca9061f98d878839c38cedf5ea7d22286
alejandrocrespomacias <128037138+alejandrocrespomacias@users.noreply.github.com>
2026-02-23 19:34:15 +0100

    Cambios frontend login
    Creación login, con imputs html css y ts con errores. Tengo que arreglar las rutas.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./login').then(m => m.LoginComponent)
+  },
+  {
+    path: 'registro',
+    loadComponent: () =>
+      import('./registro/registro.component').then(m => m.RegistroComponent)
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./dashboard/dashboard.component').then(m => m.DashboardComponent)
+  },
+  {
+    path: 'perfil',
+    loadComponent: () =>
+      import('./perfil/perfil.component').then(m => m.PerfilComponent)
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+];
