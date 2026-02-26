218ada506643fd74c8d85e0f6bd938b27f2005b8
Daniel <dani.alcaraz.p@gmail.com>
2026-02-26 19:18:46 +0100

    Correcciones en backend
    Se ha corregido la lógica de AuthController, ya que utilizaba el email para hacer el login, cuando realmente utiliza el ID.
    Además, también se han corregido errores de sintaxis.

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -7,11 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -19,10 +20,12 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
+        'identificador',
         'nombre',
         'apellidos',
         'email',
         'password',
+        'rol'
     ];
 
     /**
