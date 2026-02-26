39479d8ca1ccbd5f2abc52ab9f56bbcdc9db2abb
alejandrocrespomacias <128037138+alejandrocrespomacias@users.noreply.github.com>
2026-02-26 17:10:48 +0100

    Cambios frontend editarperfil
    He creado el componente editar perfil y lo he configurado con las rutas

---
+++ frontend/src/app/editarperfil/editarperfil.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editarperfil',
+  imports: [],
+  templateUrl: './editarperfil.html',
+  styleUrl: './editarperfil.css',
+})
+export class Editarperfil {
+
+}
