3ab0316651cab6173eaad62c612881997fa51527
Daniel <dani.alcaraz.p@gmail.com>
2026-03-13 20:16:18 +0100

    Archivos de carpeta "models" eliminados
    Se han eliminado los archivos dentro de la carpeta de models, así como la misma. Esto porque desde que se implementaron nunca se llegaron a utilizar.

--- frontend/src/app/models/xuxedex.ts
+++
@@ -1,8 +0,0 @@
-import { Xuxemon } from './xuxemon';
-
-export interface Xuxedex {
-  usuario_id: number;
-  nombre_entrenador: string;
-  listaXuxemons: Xuxemon[];
-  total_descubiertos: number;
-}
