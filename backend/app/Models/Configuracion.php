60b4edf7d555cc3933fcabec6fa1cd985db8e184
Daniel <dani.alcaraz.p@gmail.com>
2026-04-10 20:20:56 +0200

    Comentarios en algunos Modelos

--- backend/app/Models/Configuracion.php
+++ backend/app/Models/Configuracion.php
@@ -8,12 +8,14 @@ class Configuracion extends Model
     protected $table = 'configuracion';
     protected $fillable = ['clave', 'valor'];
 
+    //Metodo GET para obtener valor
     public static function get(string $clave, $default = null)
     {
         $registro = static::where('clave', $clave)->first();
         return $registro ? $registro->valor : $default;
     }
 
+    //Metodo SET para guardar o actualizar valores
     public static function set(string $clave, $valor): void
     {
         static::updateOrCreate(['clave' => $clave], ['valor' => $valor]);
